86e824710b4d6c3072f8cfe9a286ef184c14c548
kissa <saku.ylimaki@seamk.fi>
2023-12-04 17:29:07 +0200

    first commit

---
+++ karttajuttu/src/App.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './App.css';
+//import Services from "./components/Yhdistin";
+
+
+
+function App() {
+  const [paikkax, setPaikkaX] = useState(62.79);
+  const [paikkay, setPaikkaY] = useState(22.82);
+  //var marker = L.marker([62.79, 22.82]).addTo(map);
+  return (
+    <>
+    <div>
+    <h1>Kartta</h1>
+      <MapContainer center={[62.79, 22.82]} zoom={12}scrollWheelZoom={true}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+          
+          <Marker position={[paikkax,paikkay]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer></div>
+    </>
+  );
+}
+
+export default App;
