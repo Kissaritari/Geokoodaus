14a83e8043e7df5bddf0a92860c425ff43cba0cd
kissa <saku.ylimaki@seamk.fi>
2023-12-11 17:49:11 +0200

    livesijainti

--- karttajuttu/src/App.jsx
+++ karttajuttu/src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import './App.css';
 
 //import Services from "./components/Yhdistin";
@@ -9,21 +9,40 @@ import './App.css';
 function App() {
   const [paikkax, setPaikkaX] = useState(62.79);
   const [paikkay, setPaikkaY] = useState(22.82);
+  const [paikka,setPaikka] = useState();
   //var marker = L.marker([62.79, 22.82]).addTo(map);
+  function LocationMarker() {
+    const [position, setPosition] = useState(null)
+    const map = useMapEvents({
+      click() {
+        map.locate()
+      },
+      locationfound(e) {
+        setPosition(e.latlng)
+        map.flyTo(e.latlng, map.getZoom())
+      },
+    })
+  
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    )
+  }
   return (
     <>
     <div>
-    <h1>Kartta</h1>
+    <h1>Kartta
+      <div>
+        <h3></h3>
+      </div>
+    </h1>
       <MapContainer center={[62.79, 22.82]} zoom={12}scrollWheelZoom={true}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
-              <Marker position={[paikkax,paikkay]}>
-            <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-        </Marker>
-        </MapContainer></div>
+          <LocationMarker/>
+      </MapContainer></div>
     </>
   );
 }
